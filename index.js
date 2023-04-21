57d891f612dc887e4680b3874eb1164988d117d1
MyatKaungKyaw <33856024+MyatKaungKyaw@users.noreply.github.com>
2023-04-21 04:35:51 +0000

    implement addBook

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1:uuid } = require('uuid')
 
 let authors = [
   {
@@ -112,6 +113,15 @@ const typeDefs = `
     allBooks(author:String genre:String):[Book!]
     allAuthors:[Author!]!
   }
+
+  type Mutation{
+    addBook(
+      title:String!
+      published:Int!
+      author:String!
+      genres:[String!]!
+    ):Book
+  }
 `
 const booksByAuthor = (author,books) => books.filter(b => b.author === author)
 const booksByGenre = (genre,books) => books.filter(b => b.genres.includes(genre))
@@ -135,6 +145,19 @@ const resolvers = {
         name : a.name,
         bookCount : booksByAuthor(a.name,books).length,
       }))
+    },
+  },
+  Mutation :{
+    addBook : (root,args) => {
+      const book = {...args, id: uuid()}
+      books = books.concat(book)
+      if(!authors.some(a => a.name === book.author)){
+        authors = authors.concat({
+          name:book.author,
+          id: uuid(),
+        })
+      }
+      return book
     },
   }
 }
